378c12548bd58b0a3176686ecab28f66a635e9da
Tania_es <tane.stvs@gmail.com>
2021-06-01 01:01:53 +0200

    page cv
    - accueil
    - profil
    - intérêt
    - parcours

--- page_principal/assets/app.js
+++ page_principal/assets/app.js
@@ -1,8 +1,6 @@
-var parallax = document.getElementById("parallax")
+var parallax = $('.parallax');
 
-window.addEventListener("scroll", function(){
-    let offset = window.pageYOffset;
-    //console.log('Offset:'+offset)
-    //console.log('offset * 0.7'+ offset * 0.7)
-    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+$(window).scroll(function(){
+    let offset = $(window).scrollTop();
+    $(parallax).css('background-position-y' , offset*0.7+'px');
 })
